9db07ad5a86f2dd345db4c218d53b9fb40519545
Dmitriy Kotenko <antevertapro@gmail.com>
2023-02-10 13:29:13 -0500

    no jQuery

--- mpm-lead-tracking-mp.js
+++ mpm-lead-tracking-mp.js
@@ -1,17 +1,16 @@
-$(function getParams(selector){
+function getParams(selector) {
     console.log("selector", selector);
-    var src = $(selector).attr("src").split("?");    
-    var args = src[src.length-1];
+    var src = document.querySelector(selector).getAttribute("src").split("?");
+    var args = src[src.length - 1];
     args = args.split("&");
     var parameters = {};
-    for(var i=args.length-1; i>=0; i--)
-    {
+    for (var i = args.length - 1; i >= 0; i--) {
         var parameter = args[i].split("=");
         parameters[parameter[0]] = parameter[1];
     }
     return parameters;
-});
-	
-    var Params = getParams("#MPMLeadConversion")
-	console.log("gcid", parameters[gcid])
-	console.log("clientID", parameters[clientID])
+};
+
+var Params = getParams("#MPMLeadConversion");
+console.log("gcid", parameters[gcid]);
+console.log("clientID", parameters[clientID]);
